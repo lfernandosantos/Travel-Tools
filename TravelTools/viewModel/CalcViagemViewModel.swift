5ce46972ab1372e6022b1eb95082316b5b6d7163
lfernandosantos <lf.fernandodossantos@MacBook-Fernando.local>
2019-02-08 01:19:35 -0200

    Calc IOF and tax overlimit.

--- TravelTools/viewModel/CalcViagemViewModel.swift
+++ TravelTools/viewModel/CalcViagemViewModel.swift
@@ -9,5 +9,44 @@
 import Foundation
 
 class CalcViagemViewModel {
-    
+
+    var valorPago: Double = 0.0
+    var tipoPagamento: TipoPagamento = .dinheiro
+    var declarado: Bool = false
+    var valorExcedente: Double = 0.0
+
+
+    func setValor(_ valor: String) {
+        self.valorPago = Double(valor) ?? 0.0
+    }
+
+
+    func getResult() -> String {
+        let iof = CalculoPagamento.calcIOF(valor: valorPago, tipoPagamento)
+        return String(valorPago + iof)
+    }
+
+
+    func getExcedente() -> String {
+        valorExcedente = 0.0
+        if (CalculoPagamento.excedeLimite(valorPago)) {
+            valorExcedente = (valorPago - 500.0)
+        }
+        return "\(valorExcedente)0"
+    }
+
+
+    func getTotalExcedente() -> String {
+        var valor: Double = 0.0
+
+        if (valorExcedente > 0.0) {
+            if (declarado) {
+                valor = ( valorExcedente / 2 ) + valorExcedente
+            } else {
+                valor = valorExcedente * 2
+            }
+        }
+
+        return "\(valor)0"
+    }
 }
