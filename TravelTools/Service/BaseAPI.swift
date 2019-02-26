f30bd609de2264cd340a92de855d1d48eb316680
lfernandosantos <lf.fernandodossantos@MacBook-Fernando.local>
2019-02-25 23:55:18 -0300

    Create API base.

--- TravelTools/Service/BaseAPI.swift
+++ TravelTools/Service/BaseAPI.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+
+protocol BaseAPI {
+    var url: String { get }
+    var path: String { get }
+}
+
+enum BaseEndPoint {
+    case live
+}
+extension BaseEndPoint: BaseAPI {
+    var url: String {
+        return "http://www.apilayer.net/api/"
+    }
+
+    var path: String {
+        switch self {
+        case .live:
+            return "live?access_key=a217df6a11e53e7157b42b289d6c46de&format=1"
+        }
+    }
+
+}
+
+
+public enum ResultAPI<T, String> {
+    case success(T)
+    case failure(String)
+}
