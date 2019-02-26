a64ee64e1b7c5ad71a690e49ede4b92e61b02315
lfernandosantos <lf.fernandodossantos@MacBook-Fernando.local>
2019-02-25 23:55:45 -0300

    Create Base Service.

--- TravelTools/Service/BaseServiceAPI.swift
+++ TravelTools/Service/BaseServiceAPI.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+
+protocol BaseService {
+    func request(urlPath: URL, completion: @escaping (ResultAPI<Data, String>) -> Void)
+}
+
+
+extension BaseService {
+    func request(urlPath: URL, completion: @escaping (ResultAPI<Data, String>) -> Void) {
+        let task = URLSession.shared.dataTask(with: urlPath) { (data, response, error) in
+
+            if let error = error {
+                completion(ResultAPI.failure(error.localizedDescription))
+            } else {
+                completion(ResultAPI.success(data!))
+            }
+        }
+        task.resume()
+    }
+}
+
+
